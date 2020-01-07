1e2284aff224fd378f154e49643b07e1f9a3338e
Divyanshu1509 <53685443+Divyanshu1509@users.noreply.github.com>
2020-01-07 14:46:09 +0530

    Add files via upload

---
+++ Order Details(Pricing)/orderdetails.py
@@ -0,0 +1,55 @@
+print("CHOOSE YOUR FAVOURITE RESTAURANT")
+print("----------------------")
+print("The list of reataurants are:")
+print("1) Khana khajana")
+print("2) Olive garden")
+print("3) Karachi darbar")
+x = input("Choose your desired restaurant number: ")
+if x == "1":
+    print("The meals in the restaurant are :")
+    print("1) Hyderabadi Chicken Biryani")
+    print("2) Mushroom Munchurian")
+    print("3) Brownie")
+    a = input("Choose your desired dish number you want to order: ")
+    if a == "1":
+        print("The price of Hyderabadi Chicken Biryani:- Rs 250 ")
+    elif a == "2":
+    	print("The price of Mushroom Munchurian:- Rs 175")
+    elif a == "3":
+    	print("The price of Brownie:- Rs 100")
+elif x == "2":
+    print("The meals in the restaurant are :")
+    print("1) Chicken Fried rice")
+    print("2) French Fries")
+    print("3) Chocolate Ice Cream")
+    b = input("Choose your desired dish number you want to order: ")
+    if b == "1":
+    	print("The price of Chicken Fried rice:- Rs 180")
+    elif b == "2":
+    	print("The price of French Fries:- Rs 110")
+    elif b == "3":
+    	print("The price of Chocolate Ice Cream:- Rs 80")
+elif x == "3":
+    print("The meals in the restaurant are :")
+    print("1) Butter Nun")
+    print("2) Dal Makhani")
+    print("3) Kulfi")
+    c = input("Choose your desired dish number you want to order: ")
+    if c == "1":
+    	print("The price of Butter Nun:- Rs 35")
+    elif c == "2":
+    	print("The price of Dal Makhani:- Rs 150")
+    elif c == "3":
+    	print("The price of Kulfi:- Rs 50")
+print("\nThanks a lot for choosing our restaurent , we hope you enjoy the food and visit again.")
+print("------------------------------------------------------------")
+
+
+    
+
+
+
+
+
+
+
